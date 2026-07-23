d1709d76a8b1a787e95b08894049de71fc22b652
robertytocerva <robertytocerva2004@gmail.com>
2026-07-23 16:46:00 -0600

    feat: add Render deployment configuration

--- BackFinal/knexfile.js
+++ BackFinal/knexfile.js
@@ -10,7 +10,10 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
     migrations: {
       directory: './migrations',
     },
